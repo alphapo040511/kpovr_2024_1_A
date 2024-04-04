d4d594701918218a7ec280fbd334823cc478f5d2
alphapo05 <alphapo05@gmail.com>
2024-04-04 15:55:46 +0900

    [Feat] Prefabs 생성 RayCast 까지 진행

--- UnityProject_A_24_01/Assets/Scripts/ExCubePlayer.cs
+++ UnityProject_A_24_01/Assets/Scripts/ExCubePlayer.cs
@@ -11,23 +11,23 @@ public class ExCubePlayer : MonoBehaviour
 
     public int Point = 0;               //점수 수치
     public float checkTime = 0.0f;      //시간 체크 표시
-    
+
     public Rigidbody m_Rigidbody;       //오브젝트의 강체
-    
+
 
     void Update()
     {
 
         checkTime += Time.deltaTime;                 //시간을 누적해서 쌓는다. checkTime -> 0초, 1초, 0초, 1초
-        if(checkTime >= 1.0f)                       //1초마다 어떤 행동을 한다.
+        if (checkTime >= 1.0f)                       //1초마다 어떤 행동을 한다.
         {
             Point += 1;                             //1초마다 점수 1점을 올린다.
             checkTime = 0.0f;                       //시간을 초기화 한다.
         }
 
-         if (Input.GetKeyDown(KeyCode.Space))     //스페이스를 누를때
+        if (Input.GetKeyDown(KeyCode.Space))     //스페이스를 누를때
         {
-                                     
+
             m_Rigidbody.AddForce(transform.up * Power);             //y축으로 설정한 힘을 준다.
             Power = Random.Range(100, 200);                   // 100 ~ 200 사이의 값의 힘을 준다
         }
@@ -40,8 +40,23 @@ public class ExCubePlayer : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)            //충돌이 되었을 때
     {
-        //Debug.Log(collision.gameObject.name);
-        Point = 0;
-        gameObject.transform.position = Vector3.zero;       //플레이어를 원점으로 이동 시킨다.
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Pipe")
+        {
+            Point = 0;
+            gameObject.transform.position = Vector3.zero;       //플레이어를 원점으로 이동 시킨다.
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)                     //Trigger 통한 충돌
+    {
+        if (other.gameObject.tag == "Items")                //설정한 Tag로 Items와 충돌 했을 떄
+        {
+            Point += 10;                                    //Point 10점을 올려준다
+            Destroy(other.gameObject);                      //해당 오브젝트를 파괴 시켜준다.
+        }
     }
 }
+
+
