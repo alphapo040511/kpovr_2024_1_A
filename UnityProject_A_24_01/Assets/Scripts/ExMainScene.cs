ed65fec713a3493b4e0ef3cb596e16035384b616
alphapo05 <alphapo05@gmail.com>
2024-04-11 15:29:38 +0900

    [Feat] Scene 이동 & 버튼

---
+++ UnityProject_A_24_01/Assets/Scripts/ExMainScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExMainScene : MonoBehaviour
+{
+    public void GoToShootGame()                                  //버튼이 호출 할 함수를 제작
+    {
+        SceneManager.LoadScene("GameScene_Gun");                  //Scene 이동
+    }
+    public void GoToJumpGame()                                  //버튼이 호출 할 함수를 제작
+    {
+        SceneManager.LoadScene("GameScene_Jump");                  //Scene 이동
+    }
+    public void GoExit()                                        //버튼이 호출 할 함수를 제작
+    {
+        Application.Quit();                                         //게임 종료
+    }
+}
