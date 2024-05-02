82b8138d66a90c4a1703bf523136501b92a5a48d
alphapo05 <alphapo05@gmail.com>
2024-05-02 16:04:54 +0900

    [Feat] 수박 게임 Merge 이전까지

---
+++ UnityProject_A_24_01/Assets/Scripts/Game/CircleObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirecleObject : MonoBehaviour
+{
+    public bool isDrag;                             //마우스 Drag 판단
+    public bool isUsed;                             //사용 완료 체크
+    Rigidbody2D rigidbody2d;                        //2D 강체 선언 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isUsed = false;                                         //시작할때 사용이 안되었다고 입력
+        rigidbody2d = GetComponent<Rigidbody2D>();              //오브젝트의 강체에 접근
+        rigidbody2d.simulated = false;                          //물리 행동이 처음에는 동작하지않게 설정
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUsed)                                             //사용이 완료된 오브젝트는 업데이트 문을 그냥 돌려 보낸다. (마우스 Input 적용을 막기 위해)
+            return;
+
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);         //화면 스크린에서 유니티 Scene 공간의 좌표를 가져온다.
+
+            float leftBorder = -5.0f + transform.localScale.x / 2f;                         //반지름 만큼이동 제한
+            float rightBorder = 5.0f + transform.localScale.x / 2f;
+
+            if (mousePos.x < leftBorder) mousePos.x = leftBorder;                           //마우스 위치가 이동 제한 한곳 이상, 이하로 가면 값으 조절해서 가둔다.
+            if (mousePos.x > rightBorder) mousePos.x = rightBorder;
+
+            mousePos.y = 8;                                     //오브젝트 Y축 값 고정
+            mousePos.z = 0;                                     //오브젝트 Z축 값 고정
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);          //이 오브젝트를 마우스 위치로 리니어하게 0.2 값만큼씩 이동시켜 따라간다.
+        }
+
+        if (Input.GetMouseButtonDown(0)) Drag();                //마우스 버튼이 눌렸을때 Drag 함수 실행
+        if (Input.GetMouseButtonUp(0)) Drop();                  //마우스 버튼이 올라갈때 Drop 함수 실행
+    }
+
+    void Drag()                                                 //드래그 할 때 상태 값 함수
+    {
+        isDrag = true;                                          //드래그 중이다. (true)
+        rigidbody2d.simulated = false;                          //물리 시뮬레이션 안함 (false)
+    }
+
+    void Drop()                                                 //드랍 할 때 상태 값 함수
+    {
+        isDrag = false;                                         //드래그 중이다. (false)
+        isUsed = true;                                          //사용이 완료 되었다. (true)
+        rigidbody2d.simulated = true;                           //물리 시뮬레이션 사용함 (true)
+
+        GameObject temp = GameObject.FindWithTag("GameManager");                //Scene에서 GameManager Tag를 가지고 있는 오브젝트를 가져온다.
+        if(temp != null)                                                        //해당 오브젝트가 있을경우
+        {
+            temp.gameObject.GetComponent<GameManager>().GenObject();            //GameManager의 GenObject 함수를 호출
+        }
+    }
+}
--- UnityProject_A_24_01/Assets/Scripts/Tween/TweenColor.cs
+++ UnityProject_A_24_01/Assets/Scripts/Tween/TweenColor.cs
@@ -5,12 +5,12 @@ using DG.Tweening;
 
 public class TweenColor : MonoBehaviour
 {
-    private Renderer renderer;                              //랜더러를 선언한다.
+    private Renderer renderer1;                              //랜더러를 선언한다.
 
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<Renderer>();                //컴포넌트를 가져온다.
+        renderer1 = GetComponent<Renderer>();                //컴포넌트를 가져온다.
     }
 
     // Update is called once per frame
@@ -20,10 +20,10 @@ public class TweenColor : MonoBehaviour
         {
             Color color = new Color(Random.value, Random.value, Random.value);          //랜덤 색을 가져온다.
 
-            renderer.material.DOColor(color, 1f)                                        //랜덤색으로 1초후에 변경된다.
+            renderer1.material.DOColor(color, 1f)                                        //랜덤색으로 1초후에 변경된다.
                 .SetEase(Ease.InOutQuad);
 
-            renderer.material.DOPlay();                                                 //여러 트윈을 한꺼번에 실행 시킨다.
+            renderer1.material.DOPlay();                                                 //여러 트윈을 한꺼번에 실행 시킨다.
         }
     }
 }
